6ea39f29b33b66e94e0f5396940ac7b94fccf124
Dawid <dawid@pluscode.io>
2023-09-29 15:46:15 +0200

    minor

--- derivatives.py
+++ derivatives.py
@@ -31,20 +31,22 @@ p2_delta = 0.0001
 x = np.arange(0,5,0.001)
 y = f(x)
 
-x1 = 2
-x2 = x1 + p2_delta
+for i in range(5):
 
-y1 = f(x1)
-y2 = f(x2)
+    x1 = i
+    x2 = x1 + p2_delta
 
-approximate_derivative = (y2-y1)/(x2-x1)
-b = y2-approximate_derivative*x2
+    y1 = f(x1)
+    y2 = f(x2)
 
-def tangent_line(x):
-    return approximate_derivative*x + b
+    approximate_derivative = (y2-y1)/(x2-x1)
+    b = y2-approximate_derivative*x2
 
-to_plot = [x1-0.9, x1, x1+0.9]
-plt.plot(to_plot, [tangent_line(i) for i in to_plot])
+    def tangent_line(x, approximate_derivative):
+        return (approximate_derivative*x) + b
 
-plt.plot(x, y)
+    to_plot = [x1-0.9, x1, x1+0.9]
+    plt.plot(to_plot, [tangent_line(i, approximate_derivative) for i in to_plot])
+
+    plt.plot(x, y)
 plt.show()
